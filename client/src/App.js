adf568679e99769408bcd62b0d0484ae48eed49d
tinspham209 <tindeptrai1@gmail.com>
2020-11-04 15:18:16 +0700

    Setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import useStyles from "./styles";
 
-import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import useStyles from "./styles";
+import memories from "./images/memories.png";
 
 const App = () => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getPosts());
+	}, [dispatch]);
+
 	return (
 		<Container maxWidth="lg">
 			<AppBar className={classes.appBar} position="static" color="inherit">
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
+
 const Posts = () => {
 	const classes = useStyles();
+
+	const posts = useSelector((state) => state.posts);
+	console.log("posts: ", posts);
+
 	return (
 		<>
 			<h1>Posts</h1>
