9fc34e875cca29faece492b4326ddc92009a44d1
tinspham209 <tindeptrai1@gmail.com>
2020-11-04 14:02:47 +0700

    import /posts

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -20,7 +24,7 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() =>
-		app.listen([PORT, () => console.log(`Server running on port: ${PORT}`)])
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 	)
 	.catch((error) => console.log("error", error.message));
 
