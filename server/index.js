7785445b5c1deff7f9dcbffddd1d54ac3b748894
tinspham209 <tindeptrai1@gmail.com>
2020-11-04 15:18:48 +0700

    fix bug getPosts before cors

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
 	"mongodb+srv://tinspham209:tinspham209@cluster0.t1wcu.mongodb.net/<dbname>?retryWrites=true&w=majority";
