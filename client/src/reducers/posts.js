a86cc93905dc19fdb706ba5ae771770dfb886dd3
tinspham209 <tindeptrai1@gmail.com>
2020-11-07 21:09:07 +0700

    updatePost func

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,7 +4,10 @@ const reducer = (posts = [], action) => {
 			return action.payload;
 		case "CREATE":
 			return [...posts, action.payload];
-
+		case "UPDATE":
+			return posts.map((post) =>
+				post._id === action.payload._id ? action.payload : post
+			);
 		default:
 			return posts;
 	}
