209e8f40fdad35651361ee9eee23aad3fb0e8a9b
tinspham209 <tindeptrai1@gmail.com>
2020-11-07 22:21:51 +0700

    fix style of post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -42,14 +42,14 @@ const Post = ({ post, setCurrentId }) => {
 			</div>
 			<div className={classes.details}>
 				<Typography variant="body2" color="textSecondary">
-					{post.tags.map((tag) => `# ${tag}`)}
+					{post.tags.map((tag) => `#${tag} `)}
 				</Typography>
 			</div>
 			<Typography className={classes.title} variant="h5" gutterBottom>
 				{post.title}
 			</Typography>
 			<CardContent>
-				<Typography variant="h5" gutterBottom>
+				<Typography variant="body2" color="textSecondary" component="p">
 					{post.message}
 				</Typography>
 			</CardContent>
@@ -60,7 +60,7 @@ const Post = ({ post, setCurrentId }) => {
 					onClick={() => dispatch(likePost(post._id))}
 				>
 					<ThumbUpAlt fontSize="small" />
-					Like
+					&nbsp;Like &nbsp;
 					{post.likeCount}
 				</Button>
 				<Button
@@ -69,7 +69,7 @@ const Post = ({ post, setCurrentId }) => {
 					onClick={() => dispatch(deletePost(post._id))}
 				>
 					<Delete fontSize="small" />
-					Delete
+					&nbsp;Delete
 				</Button>
 			</CardActions>
 		</Card>
