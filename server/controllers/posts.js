5f3ada79135cd498e51da75bafc8ba24face1409
tinspham209 <tindeptrai1@gmail.com>
2020-11-07 20:55:12 +0700

    updatePost func

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -21,4 +22,16 @@ export const createPost = async (req, res) => {
 	} catch (error) {
 		res.status(409).json({ message: error.message });
 	}
+};
+
+export const updatePost = async (req, res) => {
+	const { id: _id } = req.params;
+	const post = req.body;
+	if (!mongoose.Types.ObjectId.isValid(_id)) {
+		return res.status(404).send("No post with that id");
+	}
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(_id, { new: true });
+
+	res.json(updatedPost);
 };
