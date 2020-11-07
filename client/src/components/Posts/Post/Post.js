5d93f7b84048ce9689ef9f73fe6005f7a26fe169
tinspham209 <tindeptrai1@gmail.com>
2020-11-07 20:40:00 +0700

    Post structure

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,59 @@
 import React from "react";
+import moment from "moment";
+
 import useStyles from "./styles";
-const Post = () => {
+import {
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+} from "@material-ui/core";
+import { ThumbUpAlt, Delete, MoreHoriz } from "@material-ui/icons";
+
+const Post = ({ post }) => {
 	const classes = useStyles();
 	return (
-		<div>
-			<h1>POST</h1>
-		</div>
+		<Card className={classes.card}>
+			<CardMedia
+				className={classes.media}
+				image={post.selectedFile}
+				title={post.title}
+			/>
+			<div className={classes.overlay}>
+				<Typography variant="h6">{post.creator}</Typography>
+				<Typography variant="body2">
+					{moment(post.createdAt).fromNow()}
+				</Typography>
+			</div>
+			<div className={classes.overlay2}>
+				<Button style={{ color: "white" }} size="small" onClick={() => {}}>
+					<MoreHoriz fontSize="default" />
+				</Button>
+			</div>
+			<div className={classes.details}>
+				<Typography variant="body2" color="textSecondary">
+					{post.tags.map((tag) => `# ${tag}`)}
+				</Typography>
+			</div>
+			<CardContent>
+				<Typography className={classes.title} variant="h5" gutterBottom>
+					{post.message}
+				</Typography>
+			</CardContent>
+			<CardActions className={classes.cardActions}>
+				<Button size="small" color="primary" onClick={() => {}}>
+					<ThumbUpAlt fontSize="small" />
+					Like
+					{post.likeCount}
+				</Button>
+				<Button size="small" color="primary" onClick={() => {}}>
+					<Delete fontSize="small" />
+					Delete
+				</Button>
+			</CardActions>
+		</Card>
 	);
 };
 
