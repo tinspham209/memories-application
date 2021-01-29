00997a48e2d1ef2c2f8449c5adcb51051c35fb07
tinspham209 <tindeptrai1@gmail.com>
2021-01-29 17:48:16 +0700

    build(index): Export component

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Auth } from "./Auth/Auth";
+export { default as Form } from "./Form/Form";
+export { default as Home } from "./Home/Home";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Posts } from "./Posts/Posts";
