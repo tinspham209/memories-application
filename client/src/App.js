2488bd88a4eac2ac67d9f7992a0918d9d74e2c9d
tinspham209 <tindeptrai1@gmail.com>
2021-01-29 17:48:35 +0700

    refactor(client): Refactor with Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import useStyles from "./styles";
-
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import memories from "./images/memories.png";
+import { Container } from "@material-ui/core";
+import { Auth, Home, Navbar } from "./components";
 
 const App = () => {
-	const classes = useStyles();
-	const dispatch = useDispatch();
-
-	const [currentId, setCurrentId] = useState(null);
-
-	useEffect(() => {
-		dispatch(getPosts());
-	}, [currentId, dispatch]);
-
 	return (
-		<Container maxWidth="lg">
-			<AppBar className={classes.appBar} position="static" color="inherit">
-				<Typography className={classes.heading} variant="h2" align="center">
-					Memories
-				</Typography>
-				<img
-					className={classes.image}
-					src={memories}
-					alt="memories"
-					height="60"
-				/>
-			</AppBar>
-			<Grow in>
-				<Container>
-					<Grid
-						className={classes.mainContainer}
-						container
-						justify="space-between"
-						alignItems="stretch"
-						spacing={3}
-					>
-						<Grid item xs={12} sm={7}>
-							<Posts setCurrentId={setCurrentId} />
-						</Grid>
-						<Grid item xs={12} sm={4}>
-							<Form currentId={currentId} setCurrentId={setCurrentId} />
-						</Grid>
-					</Grid>
-				</Container>
-			</Grow>
-		</Container>
+		<BrowserRouter>
+			<Container maxWidth="lg">
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/auth" exact component={Auth} />
+				</Switch>
+			</Container>
+		</BrowserRouter>
 	);
 };
 
