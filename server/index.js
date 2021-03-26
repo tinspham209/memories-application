8181ac9470e9bb62a5eca2dac048bda8914925d1
tinspham209 <tindeptrai1@gmail.com>
2021-03-26 21:25:53 +0700

    build(server): JWT Login & JWT Auth Backend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -14,21 +15,22 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
-	res.send("Hello to Memories Application API");
+  res.send("Hello to Memories Application API");
 });
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.CONNECTION_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() =>
-		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-	)
-	.catch((error) => console.log("error", error.message));
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log("error", error.message));
 
 mongoose.set("useFindAndModify", false);
---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  id: { type: String },
+});
+
+export default mongoose.model("User", userSchema);
