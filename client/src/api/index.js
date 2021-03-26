6a1d49ef878de7b13b940b639b03ceb584f36fe6
tinspham209 <tindeptrai1@gmail.com>
2021-03-26 20:35:14 +0700

    feat(Client): create Authentication with Google OAuth

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-const url = "https://memories-application-server.herokuapp.com/posts";
+// const url = "https://memories-application-server.herokuapp.com/posts";
+const url = "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
 
 export const createPost = (newPost) => axios.post(url, newPost);
 
 export const updatePost = (id, updatedPost) => {
-	axios.patch(`${url}/${id}`, updatedPost);
+  axios.patch(`${url}/${id}`, updatedPost);
 };
 export const deletePost = (id) => {
-	axios.delete(`${url}/${id}`);
+  axios.delete(`${url}/${id}`);
 };
 
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
